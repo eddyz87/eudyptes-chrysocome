dcf764bc0fcd95df93a73c03c74ccd2251d7efc6
black220sun <shulepov.vlad@gmail.com>
2021-07-10 14:58:48 +0300

    Rework visualized. Add stepping over solution

--- app/visualizer.lisp
+++ app/visualizer.lisp
@@ -15,6 +15,7 @@
 
 (in-package :icfpc2021/visualizer)
 
+(defparameter *svg-dir* (asdf:system-relative-pathname :icfpc2021 "../svg/"))
 
 (defmacro with-svg-to-stream ((svg &rest svg-attributes)
                               (stream)
@@ -53,45 +54,29 @@
 ;;    :file (asdf:system-relative-pathname 'icfpc2021 "visualizer/index.html")
 ;;    :content-type "text/html")
 
-(defun row-data-for-problem (problem-file)
-  (ematch (parse-json-file problem-file)
-    ((problem :hole hole :figure figure)
-     (multiple-value-bind (solution dislikes massacred-problem)
-         (ignore-errors
-           (icfpc2021/solver::solve-file problem-file
-                                         :max-iters 1000))
-       
-       (let ((message (if solution
-                          (format nil "~A<br/>Dislikes: ~A~%" problem-file dislikes)
-                          (format nil "~A<br/>~%Failed ~A~%" problem-file dislikes))))
-         ;;(format t "~A~%" message)
-         (values (hole->svg-string hole)
-                 (figure->svg-string figure)
-                 (when solution (solution->svg-string massacred-problem))
-                 message))))))
-
-(defun html-row-for-problem (problem-file)
-  (multiple-value-bind (hole-str figure-str solution-str message)
-      (row-data-for-problem problem-file)
+(defun html-row-for-solution (solution)
+  (destructuring-bind (hole-str figure-str solution-str message) solution
     (html (:tr (:td (:princ hole-str))
-	       (:td (:princ figure-str))
-	       (:td (:princ solution-str))
-	       (:td (:princ message))))))
+               (:td (:princ figure-str))
+               (:td (:princ solution-str))
+               (:td (:princ message))))))
 
-(defun problems-table (problems-dir)
+(defun problems-table (solutions)
   (html ((:table :bgcolor "white"
 		 :bordercolor "black"
 		 :border "1" :cellpadding "5"
 		 :cellspacing "1"
 		 :text-align "center")
-	 (mapcar #'html-row-for-problem
-                 (sort-problems (uiop:directory-files problems-dir))))))
+         (mapcar #'html-row-for-solution solutions))))
+
+(defun problem-id (file)
+  (cl-ppcre:register-groups-bind (num-str)
+      ("problem_([0-9]+)" (pathname-name file))
+    (parse-integer num-str)))
 
 (defun sort-problems (files)
   (loop :for path :in files
-        :for num := (cl-ppcre:register-groups-bind (num-str)
-                        ("problem_([0-9]+)" (pathname-name path))
-                      (parse-integer num-str))
+        :for num := (problem-id path)
         :if num
           :collect (cons num path) :into num-problems
         :else
@@ -99,13 +84,61 @@
         :finally (return (append (mapcar #'cdr (sort num-problems #'< :key #'car))
                                  (sort non-num-problems #'string< :key #'namestring)))))
 
-(defun visualizer-main (&key (port 8888))
-  (publish
-   :path "/" 
-   :content-type "text/html"
-   :function (lambda (req ent)
-	       (with-http-response (req ent)
-		 (with-http-body (req ent)
-		   (problems-table (asdf:system-relative-pathname 'icfpc2021 "../problems/"))))))
+(defun solve-all (svg-freq)
+  (loop :for problem-file :in (sort-problems (uiop:directory-files (asdf:system-relative-pathname :icfpc2021 "../problems/")))
+     :collect (ematch (parse-json-file problem-file)
+                ((problem :hole hole :figure figure)
+                 (multiple-value-bind (solution dislikes massacred-problem)
+                     (ignore-errors
+                       (icfpc2021/solver::solve-file problem-file
+                                                     :max-iters 1000
+                                                     :svg-prefix (format nil "~A~A" *svg-dir* (problem-id problem-file))
+                                                     :svg-freq svg-freq))
+                   
+                   (let ((message (if solution
+                                      (format nil "~A<br/>Dislikes: ~A~%" problem-file dislikes)
+                                      (format nil "~A<br/>~%Failed ~A~%" problem-file dislikes))))
+                     ;;(format t "~A~%" message)
+                     (list (hole->svg-string hole)
+                           (figure->svg-string figure)
+                           (if solution
+                               (solution->svg-string massacred-problem)
+                               (format nil "<a href=\"/~A_0\">Run</a>" (problem-id problem-file)))
+                           message)))))))
+
+(defun visualizer-main (&key (port 8888) (svg-freq 100))
+  (ensure-directories-exist *svg-dir*)
+  (let ((solutions (solve-all svg-freq)))
+    (publish
+     :path "/" 
+     :content-type "text/html"
+     :function (lambda (req ent)
+                 (with-http-response (req ent)
+                   (with-http-body (req ent)
+                     (problems-table solutions))))))
+  (publish-solutions svg-freq)
   (net.aserve:start :port port))
 
+(defun publish-solutions (svg-freq)
+  (loop :for file :in (uiop:directory-files *svg-dir*)
+     :for groups := (cl-ppcre:register-groups-bind (p s)
+                        ("([0-9])+_([0-9]{5})" (pathname-name file))
+                      (cons p (parse-integer s)))
+     :when groups
+     :do (publish
+          :path (format nil "/~A_~A" (car groups) (cdr groups))
+          :content-type "text/html"
+          :function (let ((file file)
+                          (groups groups))
+                      (lambda (req ent)
+                        (with-http-response (req ent)
+                          (with-http-body (req ent)
+                            (html ((:div :display "flex")
+                                   ((:div :width "10%")
+                                    ((:a :href (format nil "/~A_~A" (car groups) (- (cdr groups) svg-freq)))
+                                     (:princ "Prev")))
+                                   ((:div :width "80%")
+                                    (:princ (uiop:read-file-string file)))
+                                   ((:div :width "10%")
+                                    ((:a :href (format nil "/~A_~A" (car groups) (+ (cdr groups) svg-freq)))
+                                     (:princ "Next"))))))))))))
