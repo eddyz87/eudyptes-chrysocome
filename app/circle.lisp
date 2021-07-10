3f6114ca43c79e53bf7b64038fee766fa74f1cf9
eddy <eddyz87@gmail.com>
2021-07-10 20:25:54 +0300

    removed remove-duplicates

--- app/circle.lisp
+++ app/circle.lisp
@@ -13,7 +13,7 @@
 (defun build-matrix (r)
   "Accepts radius of the biggest circle possible"
   (let* ((r (1+ r))
-         (m (make-array (list r r) :element-type 'integer)))
+         (m (make-array (list r r) :element-type '(unsigned-byte 64))))
     (loop :for x :from 0 :below r
        :do (loop :for y :from 0 :below r
               :do (setf (aref m x y) (sum-sqr x y))))
@@ -21,13 +21,17 @@
 
 (defun matrix->hash-table (m)
   (let ((ht (make-hash-table)))
-    (loop :for x :from 0 :below (array-dimension m 0)
-       :do (loop :for y :from 0 :below (array-dimension m 1)
-              :for r2 := (aref m x y)
-              :do (push (cons x y) (gethash r2 ht))
-              :do (push (cons x (- y)) (gethash r2 ht))
-              :do (push (cons (- x) y) (gethash r2 ht))
-              :do (push (cons (- x) (- y)) (gethash r2 ht))))
+    (loop :for x :below (array-dimension m 0)
+          :do (loop :for y :below (array-dimension m 1)
+                    :for r2 := (aref m x y)
+                    :do (progn
+                          (push (cons x y) (gethash r2 ht))
+                          (unless (= y 0)
+                            (push (cons x (- y)) (gethash r2 ht)))
+                          (unless (= x 0)
+                            (push (cons (- x) y) (gethash r2 ht)))
+                          (when (and (/= x 0) (/= y 0))
+                            (push (cons (- x) (- y)) (gethash r2 ht))))))
     ht))
 
 (defun make-ring-hash-table (r)
@@ -37,12 +41,10 @@
 (defun vectors-in-ring (r2 eps &optional ht)
   "Accepts R^2 of the middle circle"
   (let* ((min-r2 (ceiling (* r2 (- 1 eps))))
-         (max-r2 (floor (* r2 (1+ eps))))
+         (max-r2 (floor (* r2 (+ 1 eps))))
          (ht (or ht (make-ring-hash-table (ceiling (sqrt max-r2))))))
-    (remove-duplicates
-     (loop :for r :from min-r2 :to max-r2
-        :append (gethash r ht))
-     :test #'equal)))
+    (loop :for r :from min-r2 :to max-r2
+          :append (gethash r ht))))
 
 ;; TODO: filter by min/max?
 (defun points-in-ring (p r2 eps &optional ht)
